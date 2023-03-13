afe880e16c07295a87c8c6f3e78933e8bd6f47ce
Panajotu <panajotu.andrei@gmail.com>
2023-03-13 20:08:46 +0100

    Already finished

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -2,30 +2,90 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    name: 'Pana',
+    job: 'Dipl. Oec',
+    img: 'http://aurora.ekof.bg.ac.rs/~s200555/CV.jpg',
+    text: "Mlad i neiskusan, entuzijastican i spreman za rad na karburatorima, mekdnoaldsu ili u Nargila Baru!",
   },
   {
     id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
-    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    name: 'Vuk Krstic',
+    job: 'Dipl. Oec.',
+    img: 'http://aurora.ekof.bg.ac.rs/~s201105/1647734313806.jpg',
+    text: 'Freestyle wrestler and former folkstyle wrestler who currently competes at 79 kilograms, and previously competed at 74 kilos',
   },
   {
     id: 3,
-    name: 'peter jones',
-    job: 'intern',
-    img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
-    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    name: 'Aljosa Tasic',
+    job: 'Dipl. Oec.',
+    img: 'http://aurora.ekof.bg.ac.rs/~s200543/images/IMG_9630.PNG',
+    text: "ALJOSA. ALJOSA KATAN. ALJOSA TASTATURA. ALJOSA GMAIL. ALJOSA CASOVI" ,
   },
   {
     id: 4,
-    name: 'bill anderson',
+    name: 'COBELJA',
     job: 'the boss',
-    img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
-    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    img: 'http://aurora.ekof.bg.ac.rs/~s150521/17577798_10206642521421494_21313345_n.jpg',
+    text: 'COBELJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA VEJPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP ',
   },
 ];
+
+const next = document.getElementById("next-btn");
+const prev = document.getElementById("prev-btn");
+const random = document.querySelector(".random-btn");
+let currentItem = 0;
+
+const author = document.getElementById("author");
+const img = document.getElementById("person-img");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+window.addEventListener("DOMContentLoaded", function(){
+
+showPerson(currentItem)
+
+})
+function showPerson(person){
+  const item = reviews[person];
+  console.log(item.name)
+  img.src = item.img;
+  job.textContent = item.job;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+
+next.addEventListener("click", function(){
+  currentItem ++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0
+  }
+  console.log('aa')
+  showPerson(currentItem)
+  console.log(currentItem)
+})
+prev.addEventListener("click", function(){
+  currentItem --;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+    
+  }
+  else if (currentItem<0){
+    currentItem = reviews.length - 1
+    console.log(currentItem)
+  }
+  console.log('aa')
+  showPerson(currentItem)
+})
+random.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+
+  console.log(currentItem)
+  showPerson(currentItem);
+})
+
+
+
+
+
